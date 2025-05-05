b02d60f6d78c359cb097f053863b81f3f509b5fd
unknown <huascar.rivero@ucb.edu.bo>
2025-05-05 17:20:52 -0400

    script funcional para calcular el imc y modificar html

--- calc-imc.js
+++ calc-imc.js
@@ -4,10 +4,9 @@ const estatura = document.querySelector("#estatura");
 const imc = document.querySelector("#resultado-imc")
 
 formulario.addEventListener("submit", (event) => {
-  event.preventDefault();
-
   let peso_ingresado = Number.parseFloat(peso.value);
   let estatura_ingresada = Number.parseFloat(estatura.value);
-
-  imc.textContent = peso_ingresado / (estatura_ingresada * estatura_ingresada);
+  let imc_calculado = peso_ingresado / (estatura_ingresada * estatura_ingresada);
+  console.log(imc_calculado);
+  imc.innerHTML = imc_calculado;
 });
