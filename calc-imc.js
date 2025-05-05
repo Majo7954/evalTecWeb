c4d146067102df8453de537674908958d37c7bbc
unknown <huascar.rivero@ucb.edu.bo>
2025-05-05 17:35:22 -0400

    merge con calculadora

--- calc-imc.js
+++ calc-imc.js
@@ -4,9 +4,10 @@ const estatura = document.querySelector("#estatura");
 const imc = document.querySelector("#resultado-imc")
 
 formulario.addEventListener("submit", (event) => {
-  let peso_ingresado = Number.parseFloat(peso.value);
-  let estatura_ingresada = Number.parseFloat(estatura.value);
-  let imc_calculado = peso_ingresado / (estatura_ingresada * estatura_ingresada);
-  console.log(imc_calculado);
-  imc.innerHTML = imc_calculado;
+    event.preventDefault();
+    let peso_ingresado = Number.parseFloat(peso.value);
+    let estatura_ingresada = Number.parseFloat(estatura.value);
+    let imc_calculado = peso_ingresado / (estatura_ingresada * estatura_ingresada);
+    console.log(imc_calculado);
+    imc.innerHTML = imc_calculado;
 });
